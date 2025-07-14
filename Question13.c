6053de4da0aa77d461af0b9ff243fa8915862909
Kartik <kartikshahu120@gmail.com>
2025-07-14 16:46:15 +0530

    q13

---
+++ Question13.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+#include<stdlib.h>
+int main (){
+float R,V;
+printf("Enter the Radius of the sphere : ");
+scanf("%f",&R);
+V=1.3333333333333*3.14*R*R*R;
+printf("Volume of the sphere is = %f",V);
+return 0;
+}
