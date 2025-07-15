8c6b1d927636cf8bda3334a4b8e1755d1ded6d4b
Kartik <kartikshahu120@gmail.com>
2025-07-15 19:47:10 +0530

    q17

---
+++ Question17.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main (){
+int a,b;
+printf("Enter the Value of A and B :");
+scanf("%i:%i",&a,&b);
+if(a==b)
+printf("Number 1 and number 2 are equal = %i",a);
+else
+    printf("Number 1 and number 2 are not equal");
+return 0;
+}
