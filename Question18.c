56974ca1933cd3f0e093a2aa2aa4d1fd3ea61c8c
Kartik <kartikshahu120@gmail.com>
2025-07-15 19:47:26 +0530

    q18

---
+++ Question18.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main (){
+int a;
+printf("Enter the Value of A :");
+scanf("%i",&a);
+if(a%2==0){
+printf("%i is even integer",a);}
+else{
+    printf("%i is odd integer",a);}
+return 0;
+}
