39540f959524d69f5d3338dba48763efcc333649
Kartik <kartikshahu120@gmail.com>
2025-07-18 14:18:26 +0530

    q23

---
+++ Question23.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main (){
+int m;
+printf("Enter the Value of M :");
+scanf("%i",&m);
+if(m>0)
+printf("the value of n = 1");
+else if (m==0)
+    printf("the value of n = 0");
+
+    else if (m<0)
+        printf("the value of n = -1");
+
+return 0;
+}
