652e0b4a2ae9dd19f95465554e9247677f36bb81
Kartik <kartikshahu120@gmail.com>
2025-07-19 20:13:39 +0530

    q25

---
+++ Question25.c
@@ -0,0 +1,74 @@
+#include<stdio.h>
+int main() {
+short a;
+printf("Enter :");
+scanf("%i",&a);
+switch(a){
+case 'A' :
+case 'a' :
+case 'B' :
+case 'b' :
+case 'C' :
+case 'c' :
+case 'D' :
+case 'd' :
+case 'E' :
+case 'e' :
+case 'F' :
+case 'f' :
+case 'G' :
+case 'g' :
+case 'h' :
+case 'I' :
+case 'i' :
+case 'J' :
+case 'j' :
+case 'K' :
+case 'k' :
+case 'l' :
+case 'L' :
+case 'M' :
+case 'm' :
+case 'N' :
+case 'n' :
+case 'O' :
+case 'o' :
+case 'P' :
+case 'p' :
+case 'q' :
+case 'Q' :
+case 'r' :
+case 'R' :
+case 'S' :
+case 's' :
+case 'T' :
+case 't' :
+case 'U' :
+case 'u' :
+case 'V' :
+case 'v' :
+case 'W' :
+case 'w' :
+case 'X' :
+case 'x' :
+case 'Y' :
+case 'y' :
+case 'z' :
+case 'Z' :
+case 1 :
+case 2 :
+case 3 :
+case 4 :
+case 5 :
+case 6 :
+case 7 :
+case 8 :
+case 9 :
+case 0 :
+    printf("Its not a special character");
+    break;
+    default :printf("this is a special character.");
+    break;
+}
+return 0;
+}
