043f379374845addfade9106658141b6668b4b03
Kartik <kartikshahu120@gmail.com>
2025-08-20 20:59:13 +0530

    p5

---
+++ patten5.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+
+int a,b,c,v=0,f=1,n=0;
+for(a=1;a<=4;a++){
+        for(c=a;c<=4;c++){
+                        printf(" ");
+                         }
+        for(b=1;b<=a;b++){
+                        v=f+n;
+                        n++;
+                        printf("%i ",v);
+}
+printf("\n");
+}
+}
