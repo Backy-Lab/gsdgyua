a03aae1293bbb8dd331db6115ea7bcab30dc58c2
Kartik <kartikshahu120@gmail.com>
2025-08-22 11:01:04 +0530

    q3

---
+++ Question3.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main (){
+char a,b,c;
+printf("Enter the Character 1 and Enter the Character 2 and Enter the Character 3\n");
+scanf("%c %c %c",&a,&b,&c);
+printf("The reverse Character is %C%C%C",c,b,a);
+return 0;
+}
