95b65f70c4b438d3765bc2a48799c14cc3afadcd
Kartik <kartikshahu120@gmail.com>
2025-09-23 21:14:01 +0530

    gg

---
+++ Patten7.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+
+int a,b,c,d;
+for(a=1;a<=4;a++){
+        for(c=a;c<=4;c++){
+                        printf(" ");
+                         }
+        for(b=1;b<=a;b++){
+                        printf(" %i",a);
+}
+printf("\n");
+}
+}
