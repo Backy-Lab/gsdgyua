a96dc36203f98b4876f0169f05b41edc0ed3cc3d
Kartik <kartikshahu120@gmail.com>
2025-11-19 20:47:30 +0530

    gg

---
+++ start1_q3.java
@@ -0,0 +1,13 @@
+package day01;
+
+public class start1_q3 {
+
+	public static void main(String[] args) {
+		char a='X';
+		char b='M';
+		char c='L';
+		System.out.println("ther reverse of XML is : "+c+b+a);
+
+	}
+
+}
