ff000ccaa5792288ac291793dd82d600e62cebab
Kartik <kartikshahu120@gmail.com>
2025-12-25 15:27:00 +0530

    gg

--- Question2.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-int main (){
-printf("######\n#\n#\n#####\n#\n#\n#");
-printf("\n  ######\n ##\t##\n#\n#\n#\n#\n# \n##\t##\n  ######");
-return 0;
-}
--- question1.c
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-int main (){
-printf("name: ram sharma \naddress: lucknow");
-return 0;
-}
